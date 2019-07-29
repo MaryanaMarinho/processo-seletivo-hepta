68674ad6c4b93c0c63d85c6889bfac5ae3710383
Maryana Marinho <maryanalirius@hotmail.com>
2019-07-29 18:52:10 -0300

    DAO, hashCode, equals de fabricante

--- src/com/hepta/mercado/domain/Fabricante.java
+++ src/com/hepta/mercado/domain/Fabricante.java
@@ -19,6 +19,16 @@ public class Fabricante implements Serializable {
 	
 	@Column(name = "NOME")
 	private String nome;
+	
+	
+	public Fabricante() {
+	}
+
+	public Fabricante(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
 
 	public Integer getId() {
 		return id;
@@ -35,5 +45,32 @@ public class Fabricante implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fabricante other = (Fabricante) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 
 }
