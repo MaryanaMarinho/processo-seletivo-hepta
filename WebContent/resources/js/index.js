ebf4decdbbe24b7b204784cf20bb5178bf82e06f
Maryana Marinho <maryanalirius@hotmail.com>
2019-08-03 15:39:32 -0300

    front: Inclusao de navbar, formulario cadastro de produto, lista dos formularios

--- WebContent/resources/js/index.js
+++ WebContent/resources/js/index.js
@@ -1,29 +1,73 @@
 var inicio = new Vue({
 	el:"#inicio",
-    data: {
-        listaProdutos: [],
-        listaProdutosHeader: [
-			{sortable: false, key: "nome", label:"Nome"},
-			{sortable: false, key: "fabricante.nome", label:"Fabricante"},
-			{sortable: false, key: "volume", label:"Volume"},
-			{sortable: false, key: "unidade", label:"Unidade"},
-			{sortable: false, key: "estoque", label:"Estoque"}
-		]
+	props: ['value'],
+    data: function() {
+		return {
+			aba: undefined,
+			produto: {},
+			listaProdutos: [],
+			listaProdutosHeader: [
+				{sortable: false, key: "nome", label:"Nome"},
+				{sortable: false, key: "fabricante.nome", label:"Fabricante"},
+				{sortable: false, key: "volume", label:"Volume"},
+				{sortable: false, key: "unidade", label:"Unidade"},
+				{sortable: false, key: "estoque", label:"Estoque"}
+			],
+			listaFabricantes: [],
+			listaFabricantesHeader: [
+				{ sortable: false, key: "id", label: "Id" },
+				{ sortable: false, key: "nome", label: "Nome" }
+			]
+		}
     },
     created: function(){
         let vm =  this;
-        vm.buscaProdutos();
+		vm.buscaProdutos();
+		vm.buscaFabricantes();
+		vm.setNavBar("saldacao");
     },
     methods:{
+		setNavBar: function(s) {
+			this.aba = s;
+		},
         buscaProdutos: function(){
 			const vm = this
 			axios.get("/mercado/rest/produtos")
 			.then(response => {
 				vm.listaProdutos = response.data;
-			}).catch(function (error) {
-				vm.mostraAlertaErro("Erro interno", "Não foi listar natureza de serviços");
-			}).finally(function() {
+			})
+			.catch(function (error) {
+				alert("Erro interno. Não foi possivel listar os produtos");
+			})
+			.finally(function() {
 			});
 		},
+		inserirProduto: function() {
+			const vm = this
+			axios.post("/mercado/rest/produtos", this.produto)
+				.then(response => {
+					alert("Sucesso! Produto cadastrado");
+				})
+				.catch(function (error) {
+					console.log(error);
+					alert("Erro interno. Erro ao cadastrar o produto ");
+				})
+				.finally(function () {
+					vm.buscaProdutos();
+				});
+		},
+		buscaFabricantes: function () {
+			const vm = this
+			axios.get("/mercado/rest/fabricantes")
+				.then(response => {
+					vm.listaFabricantes = response.data;
+				})
+				.catch(function (error) {
+					alert("Erro interno. Não foi possivel listar os fabricantes");
+				})
+				.finally(function () {
+				});
+		},
+
     }
 });
